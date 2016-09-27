7a6fbc5f66cf0ad599cc49679887de0a10c9cfa0
knguyen53 <knguyen53@vdc.csc.com>
2016-09-27 14:29:02 +0700

    react video

--- FrogAppsMobile/screens/welcome.js
+++ FrogAppsMobile/screens/welcome.js
@@ -9,12 +9,12 @@ import {
     TouchableOpacity,
 	Text
 } from 'react-native';
+var Global = require('../common/global.js');
 var NavigationBar = require('react-native-navbar');
-var Video = require('react-native-video');
 var styles = require('../stylesheets/styles');
+import Video from 'react-native-video';
 
 class WelcomeScreen extends Component{
-
     render(){
         let videourl = {uri: 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0'};
         var navigator = this.props.navigator;
@@ -26,24 +26,23 @@ class WelcomeScreen extends Component{
                 })
             },
         };
-
         var titleConfig = {
             title: 'Frog Apps',
         };
         return(
             <View style={styles.container}>
-                  <View style={styles.navigator}>
+              <View style={styles.navigator}>
                    <NavigationBar
                         style={styles.navigator}
                         title={titleConfig}
                         rightButton={rightButtonConfig} />
                 </View>
-            
+               <Video source={require('../background.mp4')}
+                        style={styles.backgroundVideo}
+                        rate={1} volume={1} muted={true}
+                        resizeMode="cover" repeat={true} key="video1" />
            </View>
-    // <Video source={{uri: "background"}}
-    //                     style={styles.backgroundVideo}
-    //                     rate={1} volume={1} muted={true}
-    //                     resizeMode="cover" repeat={true} key="video1" />
+   
                 //   <WebView
                 //     source={videourl}
                 //     javaScriptEnabled={true}
